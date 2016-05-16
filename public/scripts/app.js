40e850a4b0bb7e906bec338a4356757a044c9852
VisViva <a.vis.viva@gmail.com>
2016-05-16 05:28:55 +0500

    Variables altered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,8 +24,8 @@
   // Some basic variables
 
   var current_page = 1;
-  var records_per_page = 30;
-  var records_to_insert = 150;
+  var records_per_page = 50;
+  var records_to_insert = 5000;
 
   // Settings
 
